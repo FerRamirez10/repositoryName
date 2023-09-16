6c9a52450464f3c22d0e4257110d7439fb67d65a
BadPlaying <135449335+BadPlaying@users.noreply.github.com>
2023-09-15 20:05:23 -0500

    cambios
    hola

---
+++ Async - copia/script.js
@@ -0,0 +1,143 @@
+/*
+
+La api tiene un metodo random,
+por lo que no es necesario el endpoint
+
+function getIdDrink() {
+    const elem = document.getElementById('idDrink');
+    const idDrink = elem.value;
+    return idDrink;
+}
+
+function getEndpointWithId() {
+    const idDrink = this.getIdDrink();
+    if (!idDrink) { // '', 0, null, undefined // valores truthy , falsy
+        return '';
+    }
+    return `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+}
+
+*/
+let x;
+
+function Mostrar(cocktail) {
+    const info = cocktail.drinks[0];
+    document.getElementById('idCocktail').innerText = info.idDrink;
+    console.log(info.idDrink);
+    document.getElementById('nameCocktail').innerText = info.strDrink;
+    console.log(info.strDrink);
+    document.getElementById('catCocktail').innerText = info.strCategory;
+    console.log(info.strCategory);
+
+<<<<<<< HEAD
+    
+
+    
+=======
+>>>>>>> bfdd9acd7eed222a540883616a8306609548c44e
+    let Ingredientes = [];
+    for (let i = 1; i <= 15; i++) {
+        const ingredient = info[`strIngredient${i}`];
+        const measure = info[`strMeasure${i}`];
+        if (ingredient && measure) {
+            Ingredientes.push(`${measure} ${ingredient}`);
+        }
+    }
+
+    let agregar = '<ul>';
+    Ingredientes.forEach(ingredient => {
+        agregar += `<li>${ingredient}</li>`;
+        console.log(ingredient);
+    });
+    agregar += '</ul>';
+    const typesContainer = document.getElementById('ingCocktail');
+    typesContainer.innerHTML = agregar;
+
+    document.getElementById('insCocktail').innerText = info.strInstructions;
+    document.getElementById('display').src = info.strDrinkThumb;
+
+<<<<<<< HEAD
+    v = "favorita";
+
+    localStorage.setItem(v , info.idDrink);
+  
+=======
+>>>>>>> bfdd9acd7eed222a540883616a8306609548c44e
+}
+
+async function Buscar() {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json)
+        Mostrar(json);
+    } catch (error) {
+        console.error(error);
+        alert('Error' + error);
+    }
+}
+function AbrirFavs() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+<<<<<<< HEAD
+
+    const  V = localStorage.getItem(v);
+
+  }
+  
+  function CerrarFavs() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+    localStorage.removeItem(v);
+  }
+
+ 
+=======
+}
+
+function CerrarFavs() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+document.getElementById('Favorito').addEventListener('click', function () {
+    const idDrink = document.getElementById('idCocktail').innerText;
+    const nameCocktail = document.getElementById('nameCocktail').innerText;
+
+    // Crear un objeto con los datos del cóctel
+    const cocktailData = {
+        idDrink: idDrink,
+        nameCocktail: nameCocktail,
+    };
+>>>>>>> bfdd9acd7eed222a540883616a8306609548c44e
+
+    // Obtener la lista de cócteles favoritos del localStorage o inicializarla si no existe
+    let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+
+    // Agregar el cóctel actual a la lista de favoritos
+    favoritos.push(cocktailData);
+
+    // Guardar la lista de favoritos actualizada en localStorage
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+
+    alert('Cóctel agregado a favoritos');
+    MostrarFavoritos(); // Mostrar favoritos después de agregarlos
+});
+
+function MostrarFavoritos() {
+    const favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
+    const myList = document.getElementById('myList');
+
+    // Limpiar la lista actual antes de agregar los elementos actualizados
+    myList.innerHTML = '';
+
+    // Recorrer la lista de favoritos y mostrar cada elemento
+    favoritos.forEach(cocktailData => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<strong>ID:</strong> ${cocktailData.idDrink}, <strong>Nombre:</strong> ${cocktailData.nameCocktail}`;
+        myList.appendChild(listItem);
+    });
+}
+
+// Llamar a la función para mostrar los favoritos cuando se carga la página
+window.addEventListener('load', MostrarFavoritos);
---
+++ Async/script.js
@@ -0,0 +1,87 @@
+ /*
+
+La api tiene un metodo random,
+por lo que no es necesario el endpoint
+
+function getIdDrink() {
+    const elem = document.getElementById('idDrink');
+    const idDrink = elem.value;
+    return idDrink;
+}
+
+function getEndpointWithId() {
+    const idDrink = this.getIdDrink();
+    if (!idDrink) { // '', 0, null, undefined // valores truthy , falsy
+        return '';
+    }
+    return `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`;
+}
+
+*/
+
+function Mostrar(cocktail) {
+    const info = cocktail.drinks[0];
+    document.getElementById('idCocktail').innerText = info.idDrink;
+    console.log(info.idDrink);
+    document.getElementById('nameCocktail').innerText = info.strDrink;
+    console.log(info.strDrink);
+    document.getElementById('catCocktail').innerText = info.strCategory;
+    console.log(info.strCategory);
+
+    
+
+    
+    let Ingredientes = [];
+    for (let i = 1; i <= 15; i++) {
+        const ingredient = info[`strIngredient${i}`];
+        const measure = info[`strMeasure${i}`];
+        if (ingredient && measure) {
+            Ingredientes.push(`${measure} ${ingredient}`);
+        }
+    }
+
+    let agregar = '<ul>';
+    Ingredientes.forEach(ingredient => {
+        agregar += `<li>${ingredient}</li>`;
+        console.log(ingredient);
+    });
+    agregar += '</ul>';
+    const typesContainer = document.getElementById('ingCocktail');
+    typesContainer.innerHTML = agregar;
+
+    document.getElementById('insCocktail').innerText = info.strInstructions;
+    document.getElementById('display').src = info.strDrinkThumb;
+
+    v = 1;
+
+    localStorage.setItem(v , info.idDrink);
+  
+}
+
+async function Buscar() {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php`;
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json)
+        Mostrar(json);
+    } catch (error) {
+        console.error(error);
+        alert('Error'+error);
+    }
+}
+function AbrirFavs() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+
+    const  V = localStorage.getItem(v);
+
+  }
+  
+  function CerrarFavs() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+    localStorage.removeItem(v);
+  }
+
+ 
